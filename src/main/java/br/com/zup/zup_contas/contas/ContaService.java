9da90eb22333900578fa5504968bc97f9a8bb9d8
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-10 15:54:49 -0300

    Criar service para conta

---
+++ src/main/java/br/com/zup/zup_contas/contas/ContaService.java
@@ -0,0 +1,12 @@
+package br.com.zup.zup_contas.contas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaService {
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+}
