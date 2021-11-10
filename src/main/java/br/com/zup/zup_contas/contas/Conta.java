f8e56f30a1a17681af9fcc39bc7eefea37000ffa
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-10 15:50:03 -0300

    Criar classe conta

---
+++ src/main/java/br/com/zup/zup_contas/contas/Conta.java
@@ -0,0 +1,46 @@
+package br.com.zup.zup_contas.contas;
+
+import br.com.zup.zup_contas.contas.enuns.Status;
+import br.com.zup.zup_contas.contas.enuns.Tipo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "gerenciador_contas")
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Setter
+    @Column(nullable = false)
+    private String nome;
+    @Setter
+    @Column(nullable = false)
+    private double valor;
+    @Setter
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Tipo tipo;
+    @Setter
+    @Column(nullable = false)
+    private LocalDate dataDeVencimento;
+    @Setter
+    @Column(nullable = true)
+    private LocalDateTime dataDePagamento;
+    @Setter
+    private LocalDate dataCadastro;
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+}
