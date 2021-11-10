9e4eac5afe2a3d3984f7f7cc7057ed5866f709b9
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-10 16:43:35 -0300

    Adicionar método para verificar datas

--- src/main/java/br/com/zup/zup_contas/contas/ContaService.java
+++ src/main/java/br/com/zup/zup_contas/contas/ContaService.java
@@ -1,12 +1,24 @@
 package br.com.zup.zup_contas.contas;
 
+import br.com.zup.zup_contas.contas.enuns.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ContaService {
 
     @Autowired
     private ContaRepository contaRepository;
+
+    public Conta verificarData(Conta conta){
+        if (conta.getDataDeVencimento().isBefore(conta.getDataCadastro())){
+            conta.setStatus(Status.VENCIDA);
+        }
+        conta.setStatus(Status.AGUARDANDO);
+
+        return conta;
+    }
 
 }
