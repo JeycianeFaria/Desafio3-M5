eb28047d1c05533f218f4a06a31d84443f5026ca
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-10 16:00:58 -0300

    Criar controller para conta

---
+++ src/main/java/br/com/zup/zup_contas/contas/ContaController.java
@@ -0,0 +1,17 @@
+package br.com.zup.zup_contas.contas;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+}
