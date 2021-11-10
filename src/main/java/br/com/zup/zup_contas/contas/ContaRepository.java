cd90f81532b79f40b2c2d33568dd4c34bf0df447
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-10 18:20:37 -0300

    Adicionar método para filtro do id ser inteiro

--- src/main/java/br/com/zup/zup_contas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/zup_contas/contas/ContaRepository.java
@@ -2,5 +2,8 @@ package br.com.zup.zup_contas.contas;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ContaRepository extends CrudRepository<Conta, String> {
+    Optional<Conta> findById(int id);
 }
