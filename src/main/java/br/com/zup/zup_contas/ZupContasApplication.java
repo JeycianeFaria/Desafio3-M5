e3afcf37d4496717f4738eb3016ddc4255669f94
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-10 15:10:26 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/zup_contas/ZupContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.zup_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/zup_contas/ZupContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zup_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
