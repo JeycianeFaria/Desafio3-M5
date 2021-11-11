8772486ed33ffd8b7c9cebd04378cd2071bc751e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-11 00:18:10 -0300

    Adicionar método para manipular erros de validação

--- src/main/java/br/com/zup/zup_contas/config/ControllerAdvisor.java
+++ src/main/java/br/com/zup/zup_contas/config/ControllerAdvisor.java
@@ -4,28 +4,48 @@ import br.com.zup.zup_contas.contas.exceptions.ContaNaoEncontrada;
 import br.com.zup.zup_contas.contas.exceptions.NaoExisteCadastro;
 import br.com.zup.zup_contas.contas.exceptions.StatusPagamentoIncorreto;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestControllerAdvice
 public class ControllerAdvisor {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<MensagemErroValidacao> manipularErrosValidacao(MethodArgumentNotValidException exception) {
+        List<MensagemErroValidacao> erros = new ArrayList<>();
+
+        for (FieldError referencia : exception.getFieldErrors()) {
+            MensagemErroValidacao erroValidacao = new MensagemErroValidacao(referencia.getField(),
+                    referencia.getDefaultMessage());
+            erros.add(erroValidacao);
+        }
+
+        return erros;
+
+    }
+
     @ExceptionHandler(ContaNaoEncontrada.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public MensagemErroExcecao manipularContaNaoEncontrada(ContaNaoEncontrada exception){
+    public MensagemErroExcecao manipularContaNaoEncontrada(ContaNaoEncontrada exception) {
         return new MensagemErroExcecao(exception.getMessage());
     }
 
     @ExceptionHandler(NaoExisteCadastro.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public MensagemErroExcecao manipularNaoExisteCadastro(NaoExisteCadastro exception){
+    public MensagemErroExcecao manipularNaoExisteCadastro(NaoExisteCadastro exception) {
         return new MensagemErroExcecao(exception.getMessage());
     }
 
     @ExceptionHandler(StatusPagamentoIncorreto.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public MensagemErroExcecao manipularStatusPagamentoIncorreto(StatusPagamentoIncorreto exception){
+    public MensagemErroExcecao manipularStatusPagamentoIncorreto(StatusPagamentoIncorreto exception) {
         return new MensagemErroExcecao(exception.getMessage());
     }
 
