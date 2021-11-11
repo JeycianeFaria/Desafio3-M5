ab0545688eb590ac4558417946f29d1830369fd6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-11 14:44:49 -0300

    Adicionar método para manipular status de pagamento incorreto

--- src/main/java/br/com/zup/zup_contas/config/ControllerAdvisor.java
+++ src/main/java/br/com/zup/zup_contas/config/ControllerAdvisor.java
@@ -2,6 +2,7 @@ package br.com.zup.zup_contas.config;
 
 import br.com.zup.zup_contas.contas.exceptions.ContaNaoEncontrada;
 import br.com.zup.zup_contas.contas.exceptions.NaoExisteCadastro;
+import br.com.zup.zup_contas.contas.exceptions.StatusPagamentoIncorreto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,6 +20,12 @@ public class ControllerAdvisor {
     @ExceptionHandler(NaoExisteCadastro.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public MensagemErroExcecao manipularNaoExisteCadastro(NaoExisteCadastro exception){
+        return new MensagemErroExcecao(exception.getMessage());
+    }
+
+    @ExceptionHandler(StatusPagamentoIncorreto.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemErroExcecao manipularStatusPagamentoIncorreto(StatusPagamentoIncorreto exception){
         return new MensagemErroExcecao(exception.getMessage());
     }
 
