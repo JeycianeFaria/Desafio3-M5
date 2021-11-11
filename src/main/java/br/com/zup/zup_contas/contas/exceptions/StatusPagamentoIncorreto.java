0066ac92c4323c76b1b6dbdc91646f2d2c6f8173
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-11 14:43:12 -0300

    Criar classe exceção Status do pagamento incorreto

---
+++ src/main/java/br/com/zup/zup_contas/contas/exceptions/StatusPagamentoIncorreto.java
@@ -0,0 +1,9 @@
+package br.com.zup.zup_contas.contas.exceptions;
+
+public class StatusPagamentoIncorreto extends RuntimeException {
+
+    public StatusPagamentoIncorreto(String message) {
+        super(message);
+    }
+
+}
