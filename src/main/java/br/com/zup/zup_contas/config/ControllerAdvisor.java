fc5226bd47b84b019600d488ae022f0b3590ac94
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-11 11:21:51 -0300

    Adicionar método para manipular não existe cadastro

--- src/main/java/br/com/zup/zup_contas/config/ControllerAdvisor.java
+++ src/main/java/br/com/zup/zup_contas/config/ControllerAdvisor.java
@@ -1,6 +1,7 @@
 package br.com.zup.zup_contas.config;
 
 import br.com.zup.zup_contas.contas.exceptions.ContaNaoEncontrada;
+import br.com.zup.zup_contas.contas.exceptions.NaoExisteCadastro;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,6 +13,12 @@ public class ControllerAdvisor {
     @ExceptionHandler(ContaNaoEncontrada.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public MensagemErroExcecao manipularContaNaoEncontrada(ContaNaoEncontrada exception){
+        return new MensagemErroExcecao(exception.getMessage());
+    }
+
+    @ExceptionHandler(NaoExisteCadastro.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MensagemErroExcecao manipularNaoExisteCadastro(NaoExisteCadastro exception){
         return new MensagemErroExcecao(exception.getMessage());
     }
 
