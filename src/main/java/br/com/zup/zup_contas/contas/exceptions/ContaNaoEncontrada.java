4c755c621a85fa42f1f6488f2988efe52fe93590
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-11 10:26:34 -0300

    Criar método da exceção conta não encontrada

---
+++ src/main/java/br/com/zup/zup_contas/contas/exceptions/ContaNaoEncontrada.java
@@ -0,0 +1,9 @@
+package br.com.zup.zup_contas.contas.exceptions;
+
+public class ContaNaoEncontrada extends RuntimeException{
+
+    public ContaNaoEncontrada(String message) {
+        super(message);
+    }
+
+}
