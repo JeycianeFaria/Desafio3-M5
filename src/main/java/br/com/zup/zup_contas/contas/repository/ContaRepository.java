a469e42b5ceb2aa019b625123ba5bb8e86ffdc9f
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-24 08:56:54 -0300

    Refatora repository de Conta

--- src/main/java/br/com/zup/zup_contas/contas/repository/ContaRepository.java
+++ src/main/java/br/com/zup/zup_contas/contas/repository/ContaRepository.java
@@ -1,5 +1,6 @@
-package br.com.zup.zup_contas.contas;
+package br.com.zup.zup_contas.contas.repository;
 
+import br.com.zup.zup_contas.contas.Conta;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,30 +10,5 @@ import java.util.Optional;
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
 
     Optional<Conta> findById(int id);
-
-    @Query(value = "SELECT * FROM gerenciador_contas WHERE status = :status", nativeQuery = true)
-    List<Conta> status(String status);
-
-    @Query(value = "SELECT * FROM gerenciador_contas WHERE tipo = :tipo", nativeQuery = true)
-    List<Conta> tipo(String tipo);
-
-    @Query(value = "SELECT * FROM gerenciador_contas WHERE VALOR BETWEEN :valor-10 AND :valor+10", nativeQuery = true)
-    List<Conta> valor(Double valor);
-
-    @Query(value = "SELECT * FROM gerenciador_contas WHERE status = :status AND tipo = :tipo " +
-            "AND valor BETWEEN :valor-10 AND :valor+10", nativeQuery = true)
-    List<Conta> todosFiltros(String status, String tipo, Double valor);
-
-    @Query(value = "SELECT * FROM gerenciador_contas WHERE status = :status AND tipo = :tipo", nativeQuery = true)
-    List<Conta> statusTipo(String status, String tipo);
-
-    @Query(value = "SELECT * FROM gerenciador_contas WHERE status = :status AND valor BETWEEN :valor-10 AND :valor+10",
-            nativeQuery = true)
-    List<Conta> statusValor(String status, Double valor);
-
-    @Query(value = "SELECT * FROM gerenciador_contas WHERE  tipo = :tipo AND valor BETWEEN :valor-10 AND :valor+10",
-            nativeQuery = true)
-    List<Conta> tipoValor(String tipo, Double valor);
-
 
 }
