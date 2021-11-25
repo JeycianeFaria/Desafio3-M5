179db2eac99fb70928a157df84a153a705bd9909
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-25 08:52:17 -0300

    Refatorar método buscar conta por filtro

--- src/main/java/br/com/zup/zup_contas/contas/repository/ContaCustomizadaRepository.java
+++ src/main/java/br/com/zup/zup_contas/contas/repository/ContaCustomizadaRepository.java
@@ -1,6 +1,7 @@
 package br.com.zup.zup_contas.contas.repository;
 
 import br.com.zup.zup_contas.contas.Conta;
+import br.com.zup.zup_contas.contas.enuns.Status;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -15,38 +16,26 @@ public class ContaCustomizadaRepository {
         this.entityManager = entityManager;
     }
 
-    public List<Conta> buscarContaPorFiltro(String status, String tipo, Double valor){
+    public List<Conta> buscarContaPorFiltro(String status, String tipo, Double valor) {
 
         String query = "SELECT * FROM gerenciador_contas";
         String condicao = " WHERE";
 
-        if (status != null){
-            query += condicao +  " status = :status";
+        if (status != null) {
+            query += condicao + " status = " + "\"" + status + "\"";
             condicao = " AND";
         }
 
-        if (tipo != null){
-            query += condicao + " tipo = :tipo";
+        if (tipo != null) {
+            query += condicao + " tipo = " + "\"" + tipo + "\"";
             condicao = " AND";
         }
 
-        if (valor != null){
-            query += condicao + " valor BETWEEN :valor-10 AND :valor+10";
+        if (valor != null) {
+            query += condicao + " valor BETWEEN " + valor + "-10   AND " + valor + "+10";
         }
 
         var q = entityManager.createNativeQuery(query, Conta.class);
-
-        if (status != null){
-           q.setParameter("status", status);
-        }
-
-        if (tipo != null){
-            q.setParameter("tipo", tipo);
-        }
-
-        if (valor != null){
-            q.setParameter("valor", valor);
-        }
 
         return q.getResultList();
     }
